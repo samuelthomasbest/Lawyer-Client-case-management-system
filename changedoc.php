52ee399ff1e2a2417bb7be76e0e0db9d5d2ce022
Samuel Thomas Yadesa <thomassamuel256@gmail.com>
2021-03-12 09:01:34 -0800

    Add files via upload

---
+++ changedoc.php
@@ -0,0 +1,173 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['lccmsdbaid']==0)) {
+  header('location:logout.php');
+  } else{
+    if(isset($_POST['submit']))
+  {
+
+$eid=$_GET['editid'];
+ 
+
+$insertdoc=$_FILES["file1"]["name"];
+move_uploaded_file($_FILES["file1"]["tmp_name"],"images/icon/".$_FILES["file1"]["name"]);
+ $query=mysqli_query($con,"update clientcase set insertdoc='$insertdoc' where  id='$eid'");
+ 
+
+    if ($query) {
+    $msg="client Document has been Updated.";
+
+  }
+  else
+    {
+      $msg="Something Went Wrong. Please try again";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Change Client Document</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="css/font-face.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/theme.css" rel="stylesheet" media="all">
+
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <!-- HEADER MOBILE-->
+        <?php include_once('includes/sidebar.php');?>
+        <!-- END HEADER MOBILE-->
+
+        <!-- MENU SIDEBAR-->
+        
+        <!-- END MENU SIDEBAR-->
+
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+            <!-- HEADER DESKTOP-->
+            <?php include_once('includes/header.php');?>
+            <!-- HEADER DESKTOP-->
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                          
+                            <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <strong> Change</strong>  Document
+                                     
+
+
+<br><br>
+
+                                    </div>
+                                    <div class="card-body card-block">
+                                        <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                                            <p style="font-size:16px; color:green" align="center"> <?php if($msg){
+    echo $msg;
+  }  ?> </p>
+
+  <?php
+ $eid=$_GET['editid'];
+$ret=mysqli_query($con,"select * from  clientcase where id='$eid'");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+?>
+   <a href="case-detail.php?editid=<?php echo $row['id'];?>"class="btn btn-primary btn-sm">Back</a>
+
+</br></br></br></br>
+                                              <div class="row form-group">
+                                                <div class="col col-md-3">
+                                                    <label for="text-input" class=" form-control-label">change document</label>
+                                                </div>
+                                                <div class="col-12 col-md-9">
+                                                  <input type="text" id="changedoc" name="changedoc" value="<?php  echo $row['insertdoc'];?>"  placeholder="change Document" class="form-control" required="" readonly="">
+                                              </br>
+
+                                                    <span style="color:red">*</span><input type="file" name="file1" required>
+                                                </div>
+                                            </div>
+                                                                                         
+                                            <?php } ?>
+                                          <div class="card-footer">
+                                        <p style="text-align: center;"><button type="submit" name="submit" id="submit" class="btn btn-primary btn-sm">Update Document
+                                        </button></p>
+                                        
+                                    </div>
+                                        </form>
+                                    </div>
+                                   
+                                </div>
+    
+<?php include_once('includes/footer.php');?>
+<div class="card-footer">
+              
+                </div>
+                </div>
+            </div>
+        </div>
+
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="vendor/slick/slick.min.js">
+    </script>
+    <script src="vendor/wow/wow.min.js"></script>
+    <script src="vendor/animsition/animsition.min.js"></script>
+    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
+<?php }  ?>
