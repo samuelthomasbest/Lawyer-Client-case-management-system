e362101b8e1d930621115a8564b8d952e4b16894
Samuel Thomas Yadesa <thomassamuel256@gmail.com>
2021-03-12 09:04:51 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    
+                                </div>
+                            </div>
+                        </div>
---
+++ sidebar.php
@@ -0,0 +1,34 @@
+<aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+        
+                   <h1>LCCMS</h1>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li>
+                            <a href="dashboard.php"  style="color: blue">
+                                <i class="fas fa-tachometer-alt"></i>DASHBOARD</a>
+                        </li>
+
+     <li>
+                            <a href="client-form.php"  style="color: blue">
+                                <i class="fa fa-user"></i>ADD NEW CASE</a>
+                        </li>
+   <li>
+                            <a href="manage case.php"  style="color: blue">
+                                <i class="fa fa-users"></i>MANAGE CASE</a>
+                        </li>
+
+                        
+                      <li>
+                            <a href="bwdates-reports.php"  style="color: blue">
+                                <i class="fas fa-copy"></i>COURT DATE REMINDER BY DATE</a>
+                        </li>  
+                       
+                    </ul>
+                </br></br>
+                    <img src="images/icon/Lawlogo.png" width="200" height="200" style="border:solid 0px #000">
+                </nav>
+            </div>
+        </aside>
